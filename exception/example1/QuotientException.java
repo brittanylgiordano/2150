18fd55d123c87aa4d820ed5d07463ac9b0f5ffc3
brittanylgiordano <48397665+brittanylgiordano@users.noreply.github.com>
2019-03-12 10:18:26 -0400

    Create QuotientException.java

---
+++ exception/example1/QuotientException.java
@@ -0,0 +1,11 @@
+public class QuotientException
+{
+  public static void main(String [] args)
+  {
+    java.util.Scanner input = new java.util.Scanner(System.in);
+    System.out.print("
+    
+    
+    
+   }
+}
